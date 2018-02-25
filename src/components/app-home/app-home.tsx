a3112a00adda9f8244c999db3999bb43fa07d83f
Gerrit van Huyssteen <gerrit.vanhuyssteen@britehouse.co.za>
2018-02-25 22:33:09 +0200

    Initial PWA-Stenciljs app

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,4 +1,4 @@
-import { Component } from '@stencil/core';
+import { Component, State } from '@stencil/core';
 
 
 @Component({
@@ -7,28 +7,107 @@ import { Component } from '@stencil/core';
 })
 export class AppHome {
 
+  @State() active_conversion_list = [];
+  active_conversion: string = '';
+  active_base_currency: string = '';
+  active_other_currency: string = '';
+
+  zar_idr_base: number = 1185.10;
+  idr_zar_base: number = 0.00085;
+
+  constructor() {
+    this.convertZarToIdr();
+    this.convertIdrToZar();
+
+    this.setActiveConversion('zar_idr');
+  }
+
+  private convertZarToIdr() {
+    let multipliers = [10, 20, 50, 100, 200, 250, 500, 1000, 1500, 2000, 2500, 3000];
+    this.createList(this.zar_idr_base, multipliers);
+  }
+
+  private convertIdrToZar() {
+    let multipliers = [10000, 20000, 50000, 100000, 200000, 250000, 500000, 1000000, 1500000, 2000000, 2500000, 3000000];
+    this.createList(this.idr_zar_base, multipliers);
+  }
+
+  private createList(base, multipliers) {
+    this.active_conversion_list = [];
+    multipliers.forEach(multiplier => {
+      this.active_conversion_list.push({
+        base: AppHome.formatCurrency(multiplier),
+        other: AppHome.formatCurrency(multiplier * base)
+      });
+    });
+  }
+
+  private setActiveConversion(conversion) {
+    this.active_conversion = conversion;
+
+    switch (this.active_conversion) {
+      case 'zar_idr':
+        this.active_base_currency = 'ZAR';
+        this.active_other_currency = 'IDR';
+        this.convertZarToIdr();
+      break;
+
+      case 'idr_zar':
+        this.active_base_currency = 'IDR';
+        this.active_other_currency = 'ZAR';
+        this.convertIdrToZar();
+      break;
+    }
+  }
+
+  private swap() {
+    switch (this.active_conversion) {
+      case 'zar_idr':
+        this.setActiveConversion('idr_zar');
+        break;
+
+      case 'idr_zar':
+        this.setActiveConversion('zar_idr');
+        break;
+    }
+  }
+
+  private static formatCurrency(value) {
+    return value.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1 ');
+  }
+
   render() {
     return (
       <ion-page>
         <ion-header>
-          <ion-toolbar color='primary'>
-            <ion-title>Ionic PWA Toolkit</ion-title>
+          <ion-toolbar color='dark'>
+            <ion-title>Currency Converter</ion-title>
+            <ion-buttons slot="end">
+              <ion-button onClick={() => this.swap()}>
+                <ion-icon name='swap'></ion-icon>
+              </ion-button>
+            </ion-buttons>
           </ion-toolbar>
         </ion-header>
 
         <ion-content>
-          <p>
-            Welcome to the Ionic PWA Toolkit.
-            You can use this starter to build entire PWAs all with
-            web components using Stencil and ionic/core! Check out the readme for everything that comes in this starter out of the box and
-            Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
-          </p>
 
-          <stencil-route-link url='/profile/stencil'>
-            <ion-button>
-              Profile page
-            </ion-button>
-          </stencil-route-link>
+          <ion-grid>
+
+            <ion-row>
+              <ion-col class="dark" col-6>{this.active_base_currency}</ion-col>
+              <ion-col class="light" col-6>{this.active_other_currency}</ion-col>
+            </ion-row>
+
+          {this.active_conversion_list.map(conversion => {
+            return <ion-row>
+              <ion-col class="dark" col-6>{conversion.base}</ion-col>
+              <ion-col class="light" col-6>{conversion.other}</ion-col>
+            </ion-row>;
+          })}
+
+          </ion-grid>
+
         </ion-content>
       </ion-page>
     );
--- src/components/my-app/my-app.tsx
+++ src/components/my-app/my-app.tsx
@@ -15,9 +15,9 @@ export class MyApp {
     /*
       Handle service worker updates correctly.
       This code will show a toast letting the
-      user of the PWA know that there is a 
+      user of the PWA know that there is a
       new version available. When they click the
-      reload button it then reloads the page 
+      reload button it then reloads the page
       so that the new service worker can take over
       and serve the fresh content
     */
@@ -43,9 +43,6 @@ export class MyApp {
         <main>
           <stencil-router>
             <stencil-route url='/' component='app-home' exact={true}>
-            </stencil-route>
-
-            <stencil-route url='/profile/:name' component='app-profile'>
             </stencil-route>
           </stencil-router>
         </main>
